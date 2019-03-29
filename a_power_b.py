0f399ab68c9531c83ddf1a23f29af10a2d670abd
paulacaicedo <paula.caicedo.2019@upb.edu.co>
2019-03-29 10:49:07 -0500

    Pausa en 0

--- a_power_b.py
+++ a_power_b.py
@@ -1,12 +1,19 @@
 
 
-a=int(input("Introduzca un valor: "))
-b=int(input("Introduzca la potencia: "))
 
 def a_power_b(a,b):
-    prod=1
-    for i in range(1,b+1):
-        prod=prod*a
-    print("El resultado de la potencia es: ",prod)
+    while True:
+        if a!=0:
+            prod=1
+            for i in range(1,b+1):
+                prod=prod*a
+            print("El resultado de la potencia es: ",prod)
+            a = int(input("Introduzca un valor: "))
+            b = int(input("Introduzca la potencia: "))
+        else:
+            break
+
+a=int(input("Introduzca un valor: "))
+b=int(input("Introduzca la potencia: "))
 
 a_power_b(a,b)
